d00d225d1957796a924072fa09c1fbd128266be9
Robert Hildebrand <rhildebrand@weogeo.com>
2014-03-19 16:08:52 -0700

    Code clean up and some code commenting.

--- makeTableOfContents.py
+++ makeTableOfContents.py
@@ -1,17 +1,14 @@
 import os
 import ogr
 import json
-import HILDEBRAND
 
 BASED = r'C:\Users\data\\'
 FILE_MASTER = dict()
 
 def mk_json_feature(north, south, east, west, file, exts, misc):
-    north = float(north)
-    south = float(south)
-    east = float(east)
-    west = float(west)
-
+    '''Make a polygonal JSON feature comprised of a Shapefile's bounding box
+    plus additional infromation such as the base file name, all associated 
+    extensions, and the type of WeoGeo feature it is.'''
     properties = dict()
     properties['PATH'] = './' + file.split('.')[0]
     properties['EXTS'] = exts
@@ -19,13 +16,18 @@ def mk_json_feature(north, south, east, west, file, exts, misc):
     properties['WEO_TYPE'] = 'WEO_FEATURE'
     properties['WEO_MISCELLANEOUS_FILE'] = misc
 
-    geometry = {'type': 'Polygon', 'coordinates': [((west,north), (east,north), (east,south), (west,south), (west,north))]}
+    geometry = {'type': 'Polygon', 'coordinates': [((west,north), (east,north),
+                                                    (east,south), (west,south), 
+                                                    (west,north))]}
 
     feature = {'geometry': geometry, 'type': 'Feature', 'properties': properties}
 
     return feature
 
 def mk_toc(features):
+    '''The last step in the process and handles the creation of the Look Up 
+    Table and wraps all of the Table of Contents features in the necessary 
+    JSON syntax. The file is then written out to the base directory'''
     lut_properties = {'0': 'WEOALL=WEOALL', 'WEO_TYPE': 'LOOK_UP_TABLE'}
     lut = {'geometry': None, 'type': 'Feature', 'properties': lut_properties}
     features.append(lut)
@@ -35,12 +37,16 @@ def mk_toc(features):
     geojson['name'] = 'NewFeatureType'
     geojson['features'] = features
 
-    output = open(('_ToC.json'), 'w')
-    output.write(json.dumps(geojson, sort_keys=True, indent=4, separators=(',', ': ')))
+    output = open((BASED + 'WeoGeoTableOfContents.json'), 'w')
+    output.write(json.dumps(geojson, sort_keys=True, 
+                            indent=4, separators=(',', ': ')))
     output.close()
     return
 
 def mk_file_dict(base, ext, type):
+    '''Checks to see if a base file name already exists in the master 
+    dictionary and if it does, then append its extension to the list 
+    for which it is flagged.'''
     try:
         if base in FILE_MASTER:
             FILE_MASTER[base][type].append(ext)
@@ -50,6 +56,9 @@ def mk_file_dict(base, ext, type):
         FILE_MASTER[base].update({type: [ext]})
     return
 
+# Walk through the base directory and separate data files from miscellaneous
+# files. The purpose is to have a dictionary with each key being a file name
+# and the associated values being "data" + extensions and "misc" + extensions. 
 for path, dir, files in os.walk(BASED):
     for file in files:
         base = '.'.join(file.split('.')[:1])
@@ -59,22 +68,30 @@ for path, dir, files in os.walk(BASED):
         else:
             mk_file_dict(base, ext, 'data')
 
+# After the File Dictionary is made, iterate over it and extract the MBR from 
+# each Shapefile and check to see if there are any miscellaneous files related 
+# to that Shape. If there are, then make a JSON feature for that file using
+# the MBR from its associated Shapefile. Then store all of these JSON features
+# in a list to be used later. 
 features = []
 for file in FILE_MASTER:
     driver = ogr.GetDriverByName('ESRI Shapefile')
     input_features_datasource = driver.Open(BASED + file + '.shp')
     if input_features_datasource is None:
         print 'Could not open ' + file
-        exit
+        exit(0)
     input_layer = input_features_datasource.GetLayer()
     west, east, north, south = input_layer.GetExtent()
-    feat = mk_json_feature(north, south, east, west, file, ';'.join(FILE_MASTER[file]['data']), 'No')
+    feat = mk_json_feature(north, south, east, west, file, 
+                           ';'.join(FILE_MASTER[file]['data']), 'No')
     features.append(feat)
 
-    if FILE_MASTER[file]['misc'] != []:
-        feat = mk_json_feature(north, south, east, west, file, ';'.join(FILE_MASTER[file]['misc']), 'Yes')
-        features.append(feat)
-    else:
+    try:
+        if FILE_MASTER[file]['misc'] != []:
+            feat = mk_json_feature(north, south, east, west, file, 
+                                ';'.join(FILE_MASTER[file]['misc']), 'Yes')
+            features.append(feat)
+    except KeyError:
         pass
 
 mk_toc(features)
