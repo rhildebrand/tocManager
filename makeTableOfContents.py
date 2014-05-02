624ed14cc75af5f223210246bb56ea54ec9bf9a4
Robert Hildebrand <rhildebrand@weogeo.com>
2014-05-02 16:55:36 -0700

    Added support for raster input.

--- makeTableOfContents.py
+++ makeTableOfContents.py
@@ -1,8 +1,10 @@
 import os
 import ogr
+import osr
+import gdal
 import json
+import argparse
 
-BASED = r'C:\Users\data\\'
 FILE_MASTER = dict()
 
 def mk_json_feature(north, south, east, west, file, exts, misc):
@@ -37,7 +39,7 @@ def mk_toc(features):
     geojson['name'] = 'NewFeatureType'
     geojson['features'] = features
 
-    output = open((BASED + 'WeoGeoTableOfContents.json'), 'w')
+    output = open((OPTIONS.BASEDIR + 'WeoGeoTableOfContents.json'), 'w')
     output.write(json.dumps(geojson, sort_keys=True, 
                             indent=4, separators=(',', ': ')))
     output.close()
@@ -59,44 +61,73 @@ def mk_file_dict(base, ext, type):
 # Walk through the base directory and separate data files from miscellaneous
 # files. The purpose is to have a dictionary with each key being a file name
 # and the associated values being "data" + extensions and "misc" + extensions. 
-for path, dir, files in os.walk(BASED):
-    for file in files:
-        base = '.'.join(file.split('.')[:1])
-        ext = '.'.join(file.split('.')[1:])
-        if ext not in ['shp', 'dbf', 'prj', 'shx', 'sbn', 'sbx']:
-            mk_file_dict(base, ext, 'misc')
-        else:
-            mk_file_dict(base, ext, 'data')
+def main():
+    for path, dir, files in os.walk(OPTIONS.BASEDIR):
+        for file in files:
+            # FIX_ME: Ugly, but it handles multi-extensions and directory structure.
+            base = '.'.join((os.path.join(path.replace(OPTIONS.BASEDIR, ''), file)).split('.')[:1])
+            ext = '.'.join(file.split('.')[1:])
+            if ext not in ['shp', 'dbf', 'prj', 'shx', 'sbn', 'sbx', 'tif']:
+                mk_file_dict(base.replace('\\', '/'), ext, 'misc')
+            else:
+                mk_file_dict(base.replace('\\', '/'), ext, 'data')
+    
+    # After the File Dictionary is made, iterate over it and extract the MBR from 
+    # each Shapefile and check to see if there are any miscellaneous files related 
+    # to that Shape. If there are, then make a JSON feature for that file using
+    # the MBR from its associated Shapefile. Then store all of these JSON features
+    # in a list to be used later. 
+    features = []
+    for file in FILE_MASTER:
+        if 'data' in FILE_MASTER[file]:
+            if OPTIONS.DATA_TYPE == 'vector':
+                d = ogr.GetDriverByName(OPTIONS.FILE_FORMAT)
+                input_features_datasource = d.Open(OPTIONS.BASEDIR + file + '.' + FILE_MASTER[file]['data'][0])
+                if input_features_datasource is None:
+                    print 'Could not open ' + file
+                    exit(0)
+                input_layer = input_features_datasource.GetLayer()
+                west, east, north, south = input_layer.GetExtent()
+            else:
+                d = gdal.Open(OPTIONS.BASEDIR + file + '.' + FILE_MASTER[file]['data'][0])
+                geo_info = d.GetGeoTransform()
+                data_crs = osr.SpatialReference()
+                data_crs.ImportFromWkt(d.GetProjectionRef())
+                out_crs = osr.SpatialReference()
+                out_crs.ImportFromEPSG(4326)
+                transform = osr.CoordinateTransformation(data_crs, out_crs)
+                
+                west, south, z = transform.TransformPoint(geo_info[0], geo_info[3] + d.RasterXSize*geo_info[4] + d.RasterYSize*geo_info[5])
+                east, north, z = transform.TransformPoint(geo_info[0] + d.RasterXSize*geo_info[1] + d.RasterYSize*geo_info[2], geo_info[3])
 
-# After the File Dictionary is made, iterate over it and extract the MBR from 
-# each Shapefile and check to see if there are any miscellaneous files related 
-# to that Shape. If there are, then make a JSON feature for that file using
-# the MBR from its associated Shapefile. Then store all of these JSON features
-# in a list to be used later. 
-features = []
-for file in FILE_MASTER:
-    if 'data' in FILE_MASTER[file]:
-        driver = ogr.GetDriverByName('ESRI Shapefile')
-        input_features_datasource = driver.Open(BASED + file + '.shp')
-        if input_features_datasource is None:
-            print 'Could not open ' + file
-            exit(0)
-        input_layer = input_features_datasource.GetLayer()
-        west, east, north, south = input_layer.GetExtent()
-        feat = mk_json_feature(north, south, east, west, file, 
-                            ';'.join(FILE_MASTER[file]['data']), 'No')
-        features.append(feat)
-    else:
-        feat = mk_json_feature(90.0, -90.0, 180.0, -180.0, file, 
-                               ';'.join(FILE_MASTER[file]['misc']), 'Yes')
-        features.append(feat)
-
-    try:
-        if FILE_MASTER[file]['data'] != [] and FILE_MASTER[file]['misc'] != []:
             feat = mk_json_feature(north, south, east, west, file, 
+                                ';'.join(FILE_MASTER[file]['data']), 'No')
+            features.append(feat)
+        else:
+            feat = mk_json_feature(90.0, -90.0, 180.0, -180.0, file, 
                                 ';'.join(FILE_MASTER[file]['misc']), 'Yes')
             features.append(feat)
-    except KeyError:
-        pass
+    
+        try:
+            if FILE_MASTER[file]['data'] != [] and FILE_MASTER[file]['misc'] != []:
+                feat = mk_json_feature(north, south, east, west, file, 
+                                    ';'.join(FILE_MASTER[file]['misc']), 'Yes')
+                features.append(feat)
+        except KeyError:
+            pass
 
-mk_toc(features)
+    mk_toc(features)
+
+    return
+
+if __name__ == '__main__':
+    global OPTIONS
+    parser = argparse.ArgumentParser(description='Table of Contents creator for WeoGeo Listings.')
+    parser.add_argument('-B', '--BASEDIR', required=True,
+                         help='Enter the absolute path to the base directory of the data files.')
+    parser.add_argument('-F', '--FILE_FORMAT', required=False, default='ESRI Shapefile',
+                         help='Enter the geospatial file format. (GDAL)')
+    parser.add_argument('-D', '--DATA_TYPE', required=False, default='vector',
+                         help='Enter raster or vector depending on the geospatial data.')
+    OPTIONS = parser.parse_args()
+    main()
